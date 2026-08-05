bce5301f2f14a92b797e2737aabef53796f6bdc9
carlos_costa <carloscosta.pen@gmail.com>
2026-08-05 11:20:14 -0300

    Delegate Shorthands

---
+++ Delegate Shortcuts/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+    //Func delegate
+    static Func<int, int> Square = SquareMehtod;
+
+
+
+    static int SquareMehtod(int X)
+    {
+        return X * X;
+    }
+
+    static void Main()
+    {
+        //Action delegate with methods
+        Action Print = PrintMethod;
+        Action<int> PrintInt = PrintIntMethod;
+        Action<int, int> PrintSum = PrintSumMethod;
+        void PrintMethod() 
+        {
+            Console.WriteLine("Delegate without prameter");
+        }
+        void PrintIntMethod(int X)
+        {
+            Console.WriteLine("Recived int is: " + X); 
+        }
+        void PrintSumMethod(int X, int Y) 
+        {
+            int Sum = X + Y;
+            Console.WriteLine($"Sum of {X} + {Y} is: " + Sum);
+        }
+
+        Console.WriteLine("-------------------------------------------------");
+        Console.WriteLine("Func:");
+        int Result = Square(10);
+        Console.WriteLine("Square of 10 = " + Result);
+
+        Console.WriteLine("-------------------------------------------------");
+        Console.WriteLine("Actions:");
+        Print();
+        PrintInt(10);
+        PrintSum(10, 20);
+
+
+        Console.ReadLine();
+    }
+}
