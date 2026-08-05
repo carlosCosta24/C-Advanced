7a418a80ac4441c5c768f2437fc059d46d5fb22b
carlos_costa <carloscosta.pen@gmail.com>
2026-08-05 11:38:09 -0300

    Predicate delegate

--- Delegate Shortcuts/Program.cs
+++ Delegate Shortcuts/Program.cs
@@ -15,10 +15,14 @@ class Program
 
     static void Main()
     {
+        //Define a Predicate 
+        Predicate<int> IsEvenPredicate = IsEven;
+        
         //Action delegate with methods
         Action Print = PrintMethod;
         Action<int> PrintInt = PrintIntMethod;
         Action<int, int> PrintSum = PrintSumMethod;
+
         void PrintMethod() 
         {
             Console.WriteLine("Delegate without prameter");
@@ -33,6 +37,11 @@ class Program
             Console.WriteLine($"Sum of {X} + {Y} is: " + Sum);
         }
 
+        bool IsEven(int x)
+        {
+            return x % 2 == 0;
+        }
+
         Console.WriteLine("-------------------------------------------------");
         Console.WriteLine("Func:");
         int Result = Square(10);
@@ -43,6 +52,11 @@ class Program
         Print();
         PrintInt(10);
         PrintSum(10, 20);
+
+        Console.WriteLine("-------------------------------------------------");
+        Console.WriteLine("Predicate:");
+        Console.WriteLine("Is 80 Even : " + IsEven(80));
+        
 
 
         Console.ReadLine();
