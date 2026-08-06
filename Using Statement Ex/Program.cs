50b62f02ed81c58b4d988cfb85de7b44a06b87d6
carlos_costa <carloscosta.pen@gmail.com>
2026-08-06 10:22:55 -0300

    Using Statement For Resource Management

---
+++ Using Statement Ex/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.IO;
+
+public class Program
+{
+    static void Main()
+    {
+        // using statement for database
+        string ConnectionString = "Server=your_server;Database=your_database;" +
+        "User Id=your_username;Password=your_password;";
+
+        try
+        {
+            using (SqlConnection Connection = new SqlConnection(ConnectionString))
+            {
+                Connection.Open();
+                string Query = @"select * from Employees";
+
+                using (SqlCommand Command = new SqlCommand(Query, Connection))
+                {
+                    using (SqlDataReader Reader = Command.ExecuteReader())
+                    {
+                        if (Reader.HasRows)
+                        {
+                            while (Reader.Read())
+                            {
+                                string FirstName = Reader["FirstName"].ToString();
+                                string Title = Reader["Title"].ToString();
+
+                            }
+                        }else
+                        {
+                            Console.WriteLine("No Rows Found!");
+                        }
+                    }
+                }
+            }
+
+        }catch(SqlException Error)
+        {
+            Console.WriteLine($"Error: {Error.Message}");
+        }
+        // using statement with files
+        string FilePath = "File.txt";
+        try
+        {
+            using (StreamReader Reader = new StreamReader(FilePath))
+            {
+                string Line;
+                while ((Line = Reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(Line);
+                }
+            }
+        }
+        catch (FileNotFoundException) 
+        {
+            Console.WriteLine($"File Not Found: {FilePath}");
+        }catch(IOException)
+        {
+            Console.WriteLine($"Error while Reading The File {FilePath}");
+        }
+    }
+}
+
