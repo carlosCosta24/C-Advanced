be35f5a01208d34388560c409881317e553a1ef6
carlos_costa <carloscosta.pen@gmail.com>
2026-08-27 20:29:26 -0300

    Reflection type class

---
+++ Reflection/clsType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Reflection
+{
+    public class clsType
+    {
+        public static void PrintStringType() {
+            Type StringType = typeof(string);
+
+            Console.Write ("Type Info: ");
+            Console.WriteLine($"Name: {StringType.Name}");
+            Console.WriteLine($"Full Name: {StringType.FullName}");
+            Console.WriteLine($"Is Class: {StringType.IsClass}");
+
+            Console.ReadLine();
+        }
+
+    }
+}
