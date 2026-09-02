50592052de3bb374f02e8d9bf64d87e6be97a029
carlos_costa <carloscosta.pen@gmail.com>
2026-09-02 20:35:03 -0300

    Fix Constructor bug

--- Reflection/clsCustomAttribute.cs
+++ Reflection/clsCustomAttribute.cs
@@ -1,38 +1,59 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Reflection;
 
 namespace Reflection
 {
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
-    public class clsCustomAttribute:Attribute
+    public class clsCustomAttribute : Attribute
     {
-        public string Description { get; set; }
+        public string Description { get; }
 
 
         public clsCustomAttribute(string Description)
         {
 
-            Description = this.Description;
+            this.Description = Description;
         }
     }
     [clsCustom("Program Doucumntaion")]
-    public class clsDocumintation 
+    class clsDocumintation
     {
         [clsCustom("Adding Method With 2 Parameters")]
-        public static int Add(int X, int Y) 
+        public int Add(int X, int Y)
         {
             return X + Y;
         }
         [clsCustom("Printing Add Method Result")]
-        public static void Printer()
+        public void Printer()
         {
             Console.WriteLine($"{Add(10, 20)}");
         }
     }
+    class clsExcute
+    {
+        public static void Run()
+        {
+            Type type = typeof(clsDocumintation);
+
+            object[] classAttributess = type.GetCustomAttributes(typeof(clsCustomAttribute), false);
+            foreach (clsCustomAttribute attribute in classAttributess)
+            {
+                Console.WriteLine($"Class Attribute: {attribute.Description}");
+
+            }
+            MethodInfo Add = type.GetMethod("Add");
+            object[] AddInfo = Add.GetCustomAttributes(typeof(clsCustomAttribute), false);
+            foreach (clsCustomAttribute attribute in AddInfo)
+            {
+                Console.WriteLine($"Add Method: {attribute.Description}");
+
+            }
+            Console.ReadKey();
+        }
+
+
+
+    }
+
 
 }
