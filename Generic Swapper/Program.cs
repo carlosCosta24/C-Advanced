2352944b04246be480db0892f06d4c64b03bcc46
carlos_costa <carloscosta.pen@gmail.com>
2026-09-05 20:43:08 -0300

    Generic method to swap two variables

---
+++ Generic Swapper/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Swapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Using swapper method to swap two int
+            int X = 10;
+            int Y = 25;
+            Console.WriteLine($"X and Y befor swap: X: {X}, Y: {Y}");
+            clsUtility.Swapper(ref X, ref Y);
+            Console.WriteLine($"X and Y after swap: X: {X}, Y: {Y}");
+
+            //using swapper method on strings 
+            string FirstName = "Carlos";
+            string SecondName = "Costa";
+            Console.WriteLine($"FirstName and SecondName befor swap: FirstName: {FirstName}, SecondName: {SecondName}");
+            clsUtility.Swapper(ref FirstName, ref SecondName);
+            Console.WriteLine($"FirstName and SecondName after swap: FirstName: {FirstName}, SecondName: {SecondName}");
+
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
---
+++ Generic Swapper/clsUtility.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Swapper
+{
+    public class clsUtility
+    {
+    /// <summary>
+    /// Generic method to swap to variables
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="First"></param>
+    /// <param name="Second"></param>
+    /// <returns>First as second && second as first</returns>
+        public static T Swapper<T>(ref T First, ref T Second) 
+        {
+            T Temp = First;
+            First = Second;
+            Second = Temp;
+            return Temp;
+        }
+    }
+}
