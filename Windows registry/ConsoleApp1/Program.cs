97c86404f35ebd21cec7fec0f0688368e89efe24
carlos_costa <carloscosta.pen@gmail.com>
2026-09-06 15:57:11 -0300

    Write to windows registry

---
+++ Windows registry/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            clsWriteToRegistry AddNewRegistry = new clsWriteToRegistry(@"HKEY_CURRENT_USER\SOFTWARE\HIDDEN","PraivateKey", "CARLOS COSTA");
+            AddNewRegistry.AddNewRegistry();
+        }
+    }
+}
---
+++ Windows registry/ConsoleApp1/clsWriteToRegistry.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace ConsoleApp1
+{
+    public class clsWriteToRegistry
+    {
+        string Path { get; set; }
+        string Value { get; set; }
+        string Data { get; set; }
+
+
+        public clsWriteToRegistry(string FilePath, string Value, string Data) 
+        {
+            this.Path = FilePath;
+            this.Value = Value;
+            this.Data = Data;
+        }
+
+        public void AddNewRegistry() 
+        {
+            try
+            {
+                Registry.SetValue(this.Path, this.Value, this.Data);
+                Console.WriteLine($"Data saved successfully: Value:{Value}, Data:{Data}");
+                Console.ReadKey();
+            }
+            catch (Exception Error)
+            {
+                Console.WriteLine($"Error while saving to the registry: {Error.Message}");
+                Console.ReadKey();
+
+            }
+
+        }
+    }
+}
