e7abcba6fe43887491e8456b49571d5b9ffd581d
carlos_costa <carloscosta.pen@gmail.com>
2026-09-07 18:49:21 -0300

    Hashing using sha256

---
+++ Crypto/Hashing-SHA-256/clsSHA256.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Hashing_SHA_256
+{
+    public class clsSHA256
+    {
+        public static string ConvertToHash(string Input) 
+        {
+            string Result;
+            using (SHA256 Hash = SHA256.Create()) 
+            {
+                byte[] HashedBytes = Hash.ComputeHash(Encoding.UTF8.GetBytes(Input));
+                Result = BitConverter.ToString(HashedBytes).Replace("-", "");
+            }
+            return Result;
+        }
+    }
+}
