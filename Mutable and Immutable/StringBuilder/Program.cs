51f274c9e535dd1d374fdfbb50c4641b09290077
carlos_costa <carloscosta.pen@gmail.com>
2026-09-07 18:02:50 -0300

    Stringbuilder, efficiency measure

---
+++ Mutable and Immutable/StringBuilder/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+using System.Diagnostics;
+
+
+namespace StringBuilder
+{
+    internal class Program
+    {
+        static void StringConcatenation(int Iteration)
+        {
+            string Sentance = "";
+            for (int i = 0; i < Iteration; i++)
+            {
+                Sentance += (char)i;
+            }
+
+        }
+        static void StringConcatenationUsingStringBuilder(int Iteration) 
+        {
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            for (int i = 0; i < Iteration; i++)
+            {
+                sb.Append((char)i);
+            }
+            string Result = sb.ToString();
+        }
+        static void Main(string[] args) 
+        {
+
+            int Iterations = 50000;
+
+            Stopwatch FirstStopWatch = new Stopwatch();
+            FirstStopWatch.Start();
+            StringConcatenation(Iterations);
+            FirstStopWatch.Stop();
+
+            Console.WriteLine($"Normal string concatenation time: {FirstStopWatch.ElapsedMilliseconds}ms");
+
+            Stopwatch SecondStopWatch = new Stopwatch();
+            SecondStopWatch.Start();
+            StringConcatenationUsingStringBuilder(Iterations);
+            SecondStopWatch.Stop();
+
+            Console.WriteLine($"String Builder concatenation time: {SecondStopWatch.ElapsedMilliseconds}ms");
+
+            Console.ReadKey();
+
+
+        }
+
+
+
+
+
+
+    }
+}
