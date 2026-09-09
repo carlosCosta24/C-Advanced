7ffaa2a04237056589847c69f040c2d05312f84f
carlos_costa <carloscosta.pen@gmail.com>
2026-09-08 22:43:21 -0300

    Symmetic Encrytion

---
+++ Crypto/Hashing-SHA-256/clsSymmetric.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace Hashing_SHA_256
+{
+    public class clsSymmetric
+    {
+        static string Encrypt(string plainText, string key)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                // Set the key and IV for AES encryption
+                aesAlg.Key = Encoding.UTF8.GetBytes(key);
+
+                /*
+                Here, you are setting the IV of the AES algorithm to a block of bytes 
+                with a size equal to the block size of the algorithm divided by 8. 
+                The block size of AES is typically 128 bits (16 bytes), 
+                so the IV size is 128 bits / 8 = 16 bytes.
+                 */
+                aesAlg.IV = new byte[aesAlg.BlockSize / 8];
+
+
+                // Create an encryptor
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+
+                // Encrypt the data
+                using (var msEncrypt = new System.IO.MemoryStream())
+                {
+                    using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    using (var swEncrypt = new System.IO.StreamWriter(csEncrypt))
+                    {
+                        swEncrypt.Write(plainText);
+                    }
+
+                    // Return the encrypted data as a Base64-encoded string
+                    return Convert.ToBase64String(msEncrypt.ToArray());
+                }
+            }
+        }
+
+        static string Decrypt(string cipherText, string key)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                // Set the key and IV for AES decryption
+                aesAlg.Key = Encoding.UTF8.GetBytes(key);
+                aesAlg.IV = new byte[aesAlg.BlockSize / 8];
+
+
+                // Create a decryptor
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+
+                // Decrypt the data
+                using (var msDecrypt = new System.IO.MemoryStream(Convert.FromBase64String(cipherText)))
+                using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                using (var srDecrypt = new System.IO.StreamReader(csDecrypt))
+                {
+                    // Read the decrypted data from the StreamReader
+                    return srDecrypt.ReadToEnd();
+                }
+            }
+        }
+        public static string EncreptData(string Input , string Key ) 
+        {
+            return Encrypt(Input, Key);
+
+        }
+        public static string DecreptData(string Encrypted, string Key) 
+        {
+            return Decrypt(Encrypted, Key);
+        }
+    }
+}
