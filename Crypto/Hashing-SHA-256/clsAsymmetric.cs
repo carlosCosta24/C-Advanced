1345a5c4bc9e4c900a68cc0ce9180348025e7bc3
carlos_costa <carloscosta.pen@gmail.com>
2026-09-09 23:56:51 -0300

    Asymmetric encryption

---
+++ Crypto/Hashing-SHA-256/clsAsymmetric.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Hashing_SHA_256
+{
+    public class clsAsymmetric
+    {
+
+        string PublicKey { get; set; }
+        string PrivateKey { get; set; }
+        string Message { get; set; }
+
+        public clsAsymmetric(string Public, string Message)
+        {
+            this.PublicKey = Public;
+            this.Message = Message;
+            this.PrivateKey = GeneratePrivateKey();
+        }
+        string GeneratePrivateKey()
+        {
+            string Result = "";
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+                try
+                {
+                    Result = rsa.ToXmlString(true);
+                     
+                }
+                catch (CryptographicException Exception)
+                {
+                    Console.WriteLine($"Encryption/Decryption error: {Exception.Message}");
+
+                }
+                catch (Exception Error)
+                {
+                    Console.WriteLine($"Error: {Error.Message}");
+
+                }
+            return Result;
+        }
+        public string GetPublicKey()
+        {
+            return this.PublicKey;
+        }
+
+        public string Encrypt()
+        {
+            try
+            {
+                using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+                {
+                    rsa.FromXmlString(this.PublicKey);
+                    byte[] encrypted = rsa.Encrypt(Encoding.UTF8.GetBytes(this.Message), false);
+                    return Convert.ToBase64String(encrypted);
+                }
+            }
+            catch (CryptographicException Exception) 
+            {
+                Console.WriteLine($"Encryption error: {Exception.Message}");
+                throw;
+            }
+            
+        }
+
+    }
+}
