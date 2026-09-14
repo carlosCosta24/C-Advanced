5041ce3b9dcb1c07d402b3c491e236283ffcd76b
carlos_costa <carloscosta.pen@gmail.com>
2026-09-13 23:11:11 -0300

    Thread synchronization

--- Multithreading/Multithreading Examples/Program.cs
+++ Multithreading/Multithreading Examples/Program.cs
@@ -1,15 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace Multithreading_Examples
 {
     internal class Program
     {
+        static int Counter = 0;
+        static object Lock = new object();
         static void Main(string[] args)
         {
             ////Prameterless thread
@@ -38,35 +35,59 @@ namespace Multithreading_Examples
             //        Thread.Sleep(500);
             //    }
             //}
-            
+
             //void Print(string s) 
             //{
             //    Console.WriteLine(s);
             //}
-            int DownloadWebPage(string Url) 
-            {
-                using (WebClient Client = new WebClient()) 
-                {
-                    string Content = Client.DownloadString(Url);
-                    return Content.Length;
-                }
-            }
-            Console.WriteLine("Start downloading....");
-            Thread t1 = new Thread(() => Console.WriteLine($"Amazon: {DownloadWebPage("https://www.amazon.com")}"));
+            //int DownloadWebPage(string Url)
+            //{
+            //    using (WebClient Client = new WebClient())
+            //    {
+            //        string Content = Client.DownloadString(Url);
+            //        return Content.Length;
+            //    }
+            //}
+            //Console.WriteLine("Start downloading....");
+            //Thread t1 = new Thread(() => Console.WriteLine($"Amazon: {DownloadWebPage("https://www.amazon.com")}"));
+            //t1.Start();
+
+            //Thread t2 = new Thread(() => Console.WriteLine($"Cnn: {DownloadWebPage("https://www.cnn.com")}"));
+            //t2.Start();
+
+            //Thread t3 = new Thread(() => Console.WriteLine($"Linkedin: {DownloadWebPage("https://www.Linkedin.com")}"));
+            //t3.Start();
+
+            //t1.Join();
+            //t2.Join();
+            //t3.Join();
+
+            //Console.WriteLine("All webpages have been downloaded");
+
+            Console.WriteLine("Start Counters....");
+            Thread t1 = new Thread(() => IncrementCounter());
             t1.Start();
 
-            Thread t2 = new Thread(() => Console.WriteLine($"Cnn: {DownloadWebPage("https://www.cnn.com")}"));
-            t2.Start(); 
-
-            Thread t3 = new Thread(() => Console.WriteLine($"Linkedin: {DownloadWebPage("https://www.Linkedin.com")}"));
-            t3.Start();
+            Thread t2 = new Thread(() => IncrementCounter());
+            t2.Start();
 
             t1.Join();
             t2.Join();
-            t3.Join();
-
-            Console.WriteLine("All webpages have been downloaded");
+            Console.WriteLine($"Counters 1: {Counter}");
+            Console.WriteLine($"Counters 2: {Counter}");
             Console.ReadKey();
+        }
+        static int IncrementCounter()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                lock (Lock)
+                {
+
+                    Counter++;
+                }
+            }
+            return Counter;
         }
     }
 }
