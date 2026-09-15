edecea35246a6db4d4ee87614683a073bdc18c73
carlos_costa <carloscosta.pen@gmail.com>
2026-09-15 05:03:30 -0300

    Asynchronous Programming

---
+++ Async/Asynchronous Programming/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Asynchronous_Programming
+{
+    internal class Program
+    {
+        static async Task Main()
+        {
+            Task<int> CalculationResult = PerformCalculationAsync();
+            Console.WriteLine("Calculation started>>>");
+
+            int FinalResult = await CalculationResult;
+
+            Console.WriteLine("Calculation finished>>");
+            Console.WriteLine(FinalResult);
+            Console.ReadKey();
+        }
+        static async Task<int> PerformCalculationAsync()
+        {
+            int Result = 0;
+            for (int i = 0; i < 500; i++)
+            {
+                Result += i;
+            }
+            await Task.Delay(500);
+            return Result;
+        }
+    }
+}
