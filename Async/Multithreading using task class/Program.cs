aaf126cb7dcb85c6c94b4cac0e3195f1acdf79e9
carlos_costa <carloscosta.pen@gmail.com>
2026-09-16 02:51:57 -0300

    Multithreading using task.Run

---
+++ Async/Multithreading using task class/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Multithreading_using_task_class
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Task task1 = Task.Run(() => DownloadFile("Chrome"));
+
+            Task task2 = Task.Run(() => DownloadFile("C++ config"));
+
+            await Task.WhenAll(task1, task2);
+
+            Console.WriteLine("All Download finished");
+            Console.ReadKey();
+        }
+
+        static void DownloadFile(string FileName)
+        {
+            Console.WriteLine($"Downloading file: {FileName} started");
+            Thread.Sleep(2000);
+            Console.WriteLine("Download finished");
+        }
+    }
+}
