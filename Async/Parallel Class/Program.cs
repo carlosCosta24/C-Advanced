4319a2d8e1cd6b3e4649b532f7f60bb436d7983a
carlos_costa <carloscosta.pen@gmail.com>
2026-09-17 02:39:22 -0300

    Parallel For / ForEach

---
+++ Async/Parallel Class/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parallel_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int NumberOfIterations = 10;
+            List<string> Urls = new List<string>
+            {
+                "https://www.cnn.com",
+                "https://www.amazon.com",
+                "https://www.github.com",
+
+            };
+            // parallel for
+            Parallel.For(0, NumberOfIterations, (i) =>
+            {
+                Console.WriteLine($"Iteration {i} on thread {Task.CurrentId}");
+            });
+
+            //parallel for each
+            Parallel.ForEach(Urls, url => { DownloadWebPage(url);});
+
+            Console.ReadKey();
+        }
+        static void DownloadWebPage(string Url) 
+        {
+            using (WebClient Client = new WebClient()) 
+            {
+                string Content = Client.DownloadString(Url);
+                Console.WriteLine($"{Url} char count is: {Content.Length}");
+            }
+            
+        }
+    }
+}
